5abbf4c15f153639e7683d1c3fa4943ba1e7643b
Yuzu.io <1581523609@qq.com>
2024-01-20 12:23:10 +0800

    feat:在pinia userStore中编写登录、获取用户个人信息方法

--- src/plugins/stores/modules/user.ts
+++ src/plugins/stores/modules/user.ts
@@ -1,12 +1,41 @@
+import { getUserInfoApi, loginApi } from '@/api/auth'
+import type { GetUserInfoRes, LoginParams } from '@/types/auth'
 import { defineStore } from 'pinia'
 
 export const useUserStore = defineStore('userStore', {
   state: (): State => ({
-    accessToken: ''
+    accessToken: '',
+    userInfo: {
+      id: '',
+      username: '',
+      account: '',
+      sex: '',
+      avatar: '',
+      age: 0,
+      email: '',
+      phone: '',
+      createTime: '',
+      updateTime: ''
+    }
   }),
-  actions: {}
+  actions: {
+    async login(params: LoginParams) {
+      const res = await loginApi(params).catch((e) => console.warn(e))
+      if (!res) return Promise.reject()
+      this.accessToken = res.data.token
+      return Promise.resolve()
+    },
+    async getUserInfo() {
+      const res = await getUserInfoApi().catch((e) => console.log(e))
+      if (!res) return Promise.reject()
+      this.userInfo = res.data
+      return Promise.resolve()
+    }
+  },
+  persist: true
 })
 
 interface State {
   accessToken: string
+  userInfo: GetUserInfoRes
 }
