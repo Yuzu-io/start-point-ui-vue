3eada13a3f7f9707dccf5642cbf2d4da544309e4
Yuzu.io <1581523609@qq.com>
2024-02-05 19:39:15 +0800

    refactor:修改vite config css解析scss的文件为mixin.scss

--- vite.config.ts
+++ vite.config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "@/styles/global.scss";`
+        additionalData: `@import "@/styles/mixin.scss";`
       }
     }
   }
