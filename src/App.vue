6d302f30967f60871736c52062d591c282032dde
Yuzu.io <1581523609@qq.com>
2024-02-08 14:52:52 +0800

    refactor:配置国际化以及全局加载loading

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
-  <t-config-provider :global-config="getLocale">
-    <RouterView :key="locale" />
-  </t-config-provider>
+  <a-config-provider :locale="getLocale">
+    <a-spin :spinning="loadingStore.loading" :delay="loadingStore.delay" :tip="loadingStore.tip">
+      <RouterView :key="locale" />
+    </a-spin>
+  </a-config-provider>
 </template>
 
 <script lang="ts" setup>
 import { useLocale } from './locales/useLocale'
+import { useLoadingStore } from './plugins/stores'
+
+const loadingStore = useLoadingStore()
+
 const { getLocale, locale } = useLocale()
 </script>
