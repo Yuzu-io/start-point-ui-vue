bf732c52d56f4fa3a959e552020610541a0b8c4f
Yuzu.io <1581523609@qq.com>
2024-02-08 14:52:10 +0800

    feat:编写RoutesInfo类型interface

---
+++ src/types/routes.ts
@@ -0,0 +1,17 @@
+export interface RoutesInfo {
+  id: string
+  title: string
+  icon: string
+  fullPath: string
+  componentPath: string
+  parentId: string
+  showStatus: string
+  isExternalLink: string
+  keepAlive: string
+  redirect: string
+  type: string
+  authMark: string
+  orderIndex: number
+  createTime: string
+  updateTime: string
+}
