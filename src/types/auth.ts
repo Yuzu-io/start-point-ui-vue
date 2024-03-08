85eda5329ab0648c3a14e3191895d3314bf584d5
Yuzu.io <1581523609@qq.com>
2024-03-08 18:08:50 +0800

    refactor:移除GetUserInfoRes类型

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -30,36 +30,3 @@ export interface GetValidateCodeRes {
   /*验证码value */
   codeValue: string
 }
-
-// 响应接口
-export interface GetUserInfoRes {
-  /*用户编号 */
-  id: string
-
-  /*用户名 */
-  username: string
-
-  /*账号 */
-  account: string
-
-  /*性别 */
-  sex: string
-
-  /*头像 */
-  avatar: string
-
-  /*年龄 */
-  age: number
-
-  /*邮箱 */
-  email: string
-
-  /*手机号 */
-  phone: string
-
-  /*用户登录请求参数 */
-  createTime: string
-
-  /*用户登录请求参数 */
-  updateTime: string
-}
