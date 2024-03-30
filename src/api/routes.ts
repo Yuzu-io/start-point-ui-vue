3b731026ea84d5686a6d0a4397c5ce9d89f0ba2a
Yuzu <1581523609@qq.com>
2024-03-30 22:20:47 +0800

    refactor：routes关于修改的接口添加showCodeMessage属性

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -35,37 +35,57 @@ export function getParentRoutesListApi() {
 }
 
 export function addRoutesApi(data: AddRoutesParams) {
-  return createAxios<void>({
-    url: Api.add,
-    method: 'post',
-    data
-  })
+  return createAxios<void>(
+    {
+      url: Api.add,
+      method: 'post',
+      data
+    },
+    {
+      showCodeMessage: true
+    }
+  )
 }
 
 export function editRoutesApi(data: EditRoutesParams) {
-  return createAxios<void>({
-    url: Api.edit,
-    method: 'put',
-    data
-  })
+  return createAxios<void>(
+    {
+      url: Api.edit,
+      method: 'put',
+      data
+    },
+    {
+      showCodeMessage: true
+    }
+  )
 }
 
 export function deleteRoutesApi(id: string) {
-  return createAxios<void>({
-    url: Api.delete,
-    method: 'delete',
-    params: {
-      id
+  return createAxios<void>(
+    {
+      url: Api.delete,
+      method: 'delete',
+      params: {
+        id
+      }
+    },
+    {
+      showCodeMessage: true
     }
-  })
+  )
 }
 
 export function batchDeleteRoutesApi(data: string[]) {
-  return createAxios<void>({
-    url: Api.batchDelete,
-    method: 'delete',
-    data
-  })
+  return createAxios<void>(
+    {
+      url: Api.batchDelete,
+      method: 'delete',
+      data
+    },
+    {
+      showCodeMessage: true
+    }
+  )
 }
 
 export function findByIdApi(id: string) {
