6c56903a4ff6541985553b5c719436261bbd1608
Yuzu <1581523609@qq.com>
2024-04-16 12:48:04 +0800

    feat：新增字典管理、字典数据页面模板

---
+++ src/views/system/dictData/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>字典数据</div>
+</template>
+
+<script setup lang="ts">
+import { mainRouteName } from '@/permission'
+
+defineOptions({
+  name: `${mainRouteName}-dictData`
+})
+</script>
+
+<style scoped></style>
